45a280b5438b9e10caf0d38c22a92d3d841c6073
v-dedhot <v-dedhot@AZPRDMSQL01>
2018-10-24 04:47:58 -0700

    Add project files.

---
+++ Deoyaniweb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Deoyaniweb.Startup))]
+namespace Deoyaniweb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
